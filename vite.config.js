a3bf866da5ff1115ebab1594ad4e9906efa9880e
Lucman40 <man.mdm@outlook.com>
2024-11-15 14:57:06 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,21 +2,26 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  plugins: [
-    react({
-      jsxRuntime: 'classic'
-    })
-  ],
-  esbuild: {
-    loader: 'jsx',
-    include: /\.[jt]sx?$/,
-    exclude: []
-  },
-  optimizeDeps: {
-    esbuild: {
-      loader: {
-        '.js': 'jsx'
-      }
-    }
-  }
+ plugins: [
+   react({
+     jsxRuntime: 'classic'
+   })
+ ],
+ esbuild: {
+   loader: 'jsx',
+   include: /\.[jt]sx?$/,
+   exclude: []
+ },
+ optimizeDeps: {
+   esbuild: {
+     loader: {
+       '.js': 'jsx'
+     }
+   }
+ },
+ resolve: {
+   alias: {
+     'lucide-react': 'lucide-react/dist/esm/lucide-react'
+   }
+ }
 });
